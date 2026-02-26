86d07239c92b2bbe79de5e2047ba23c74d13d1d2
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-02-27 01:42:06 +0800

    access-controls: initial roleCheck function
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- backend/access-controls/users.roles.js
+++ backend/access-controls/users.roles.js
@@ -1,12 +1,12 @@
-const roleCheck = async(role) => {
-    switch (role) {
-        case "admin":
-        	return
-        case "seller":
-        	return
-        default:
-            throw new Error(`unknown role '${role}'`)
-    }
-}
+const roleCheck = async (role) => {
+	switch (role) {
+		case "admin":
+			return;
+		case "seller":
+			return;
+		default:
+			throw new Error(`unknown role '${role}'`);
+	}
+};
 
-export { roleCheck }
+export { roleCheck };
