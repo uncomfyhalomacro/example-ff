a7d1fa25d9de5b3edc7b2bba684e4039e9fab7d2
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-02-26 20:48:04 +0800

    api: add /healthz endpoint and cors config
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import fastifyCookie from "@fastify/cookie";
+import cors from "@fastify/cors";
 import Fastify from "fastify";
 import { loadEnvFile } from "node:process";
 import handleProtectedWithLogin from "./middleware/protected.login.js";
@@ -8,8 +9,8 @@ import { updateHandler } from "./routes/auth.update.js";
 
 loadEnvFile();
 
-const PROD = process.env.PROD || "dev";
 const PORT = process.env.PORT || 3000;
+const PROD = process.env.PROD || "prod";
 const COOKIE_SECRET = process.env.COOKIE_SECRET || "cookie-secret";
 const fastify = Fastify({
 	logger: true,
@@ -19,18 +20,22 @@ fastify.register(fastifyCookie, {
 	secret: COOKIE_SECRET,
 });
 
-// fastify.get("/", async function handler(request, response) {
-// 	// Testing only if the login works. We run `curl -b cookie.jar http://localhost:$PORT/` to test
-// 	const data = await verifyJwt({ token: request.cookies.session });
-// 	return data;
-// });
+fastify.register(cors, {
+	strictPreflight: true,
+	origin: PROD === "dev" ? "http://localhost:8080" : "",
+	methods: ["GET", "HEAD", "POST", "DELETE", "PUT"],
+	allowedHeaders: ["Content-Type", "Authorization"]
+});
+
+fastify.get("/healthz", async (_, resp) => {
+    return resp.code(200).send("OK")
+})
 
 fastify.post("/register", registerHandler);
 fastify.post("/login", loginHandler);
-fastify.post("/update", async (req, resp) => {
+fastify.put("/update", async (req, resp) => {
 	await handleProtectedWithLogin(req, resp, updateHandler);
 });
-
 
 try {
 	await fastify.listen({ port: PORT });
